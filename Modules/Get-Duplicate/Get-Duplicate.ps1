b3946b80c954052e79665b7986bf562881cf86b1
Leo <leonardjonathanoh@live.com>
2018-06-14 16:47:16 +0800

    Did not deal with -LiteralPath properly again

--- Modules/Get-Duplicate/Get-Duplicate.ps1
+++ Modules/Get-Duplicate/Get-Duplicate.ps1
@@ -30,7 +30,7 @@ function Get-Duplicate {
             Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
         [ValidateScript({
-            if ((! (Test-Path -Literal $_ -PathType Container) ) #-or
+            if ((! (Test-Path -LiteralPath $_ -PathType Container) ) #-or
                 #($_.IndexOfAny([System.IO.Path]::GetInvalidFileNameChars()) -ne -1) #-or
                 #($_.IndexOfAny([System.IO.Path]::GetInvalidPathChars()) -ne -1)
                 ) {
@@ -67,7 +67,7 @@ function Get-Duplicate {
         try {
             if ($InputObject) {
                 if ($InputObject -is [array]) { 
-                    $InputObject | % {
+                    $InputObject | ForEach-Object {
                         if ($_ -is [string]) {
                             $PSBoundParameters.Remove('InputObject') > $null
                             Get-Duplicate -Path $_ @PSBoundParameters
@@ -87,7 +87,7 @@ function Get-Duplicate {
                     $fileSearchParams['Path'] = $Path
                 }
                 if ($LiteralPath) {
-                    $fileSearchParams['Path'] = $LiteralPath
+                    $fileSearchParams['LiteralPath'] = $LiteralPath
                 }
                 if ($Exclude) {
                     $fileSearchParams['Exclude'] = $Exclude
