ba6b0d7eb98f52775483018e62ee0b3ecebae5ab
Leo <leonardjonathanoh@live.com>
2019-03-11 20:15:22 +0800

    One again. Does not all powershell packages include Pester?

--- test/test.ps1
+++ test/test.ps1
@@ -4,7 +4,8 @@ Set-StrictMode -Version Latest
 $global:PesterDebugPreference_ShowFullErrors = $true
 
 # Install Pester if needed
-if ( (Get-Module Pester -ListAvailable -ErrorAction SilentlyContinue).Version.Major -lt 4 ) {
+$pester = Get-Module Pester -ListAvailable -ErrorAction SilentlyContinue
+if ( $pester -and $pester.Version.Major -lt 4 ) {
     Install-Module Pester -Force -Scope CurrentUser
 }
 Get-Module Pester -ListAvailable
