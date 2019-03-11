e85448ec82f8466742e3456b5d508721715c2478
Leo <leonardjonathanoh@live.com>
2019-03-11 20:11:12 +0800

    Ensure Get-Module check does not throw terminating error in test.ps1

--- test/test.ps1
+++ test/test.ps1
@@ -4,7 +4,7 @@ Set-StrictMode -Version Latest
 $global:PesterDebugPreference_ShowFullErrors = $true
 
 # Install Pester if needed
-if ( (Get-Module Pester -ListAvailable).Version.Major -lt 4 ) {
+if ( (Get-Module Pester -ListAvailable -ErrorAction SilentlyContinue).Version.Major -lt 4 ) {
     Install-Module Pester -Force -Scope CurrentUser
 }
 Get-Module Pester -ListAvailable
