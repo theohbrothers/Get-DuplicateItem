ccc8b97804909aaabde748cd14491dff34e77acb
Leo <leonardjonathanoh@live.com>
2019-03-11 18:37:30 +0800

    improve main test script

--- test/test.ps1
+++ test/test.ps1
@@ -1,7 +1,10 @@
 . "$( Split-Path $PSScriptRoot -Parent )/env.ps1"
 
+Set-StrictMode -Version Latest
+$global:PesterDebugPreference_ShowFullErrors = $true
+
 Get-Module "$MODULE_NAME" | Remove-Module
-Import-Module "$APP_DIR/$MODULE_NAME.psd1"
+Import-Module "$APP_DIR/$MODULE_NAME.psd1" -Force
 
 # Run tests
 $res = Invoke-Pester -Script $APP_DIR -PassThru
