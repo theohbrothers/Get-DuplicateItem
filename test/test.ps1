f1b5fb5269e53b2eebd53ec4c0018d4353a9ac5f
Leo <leonardjonathanoh@live.com>
2019-03-11 18:44:24 +0800

    install Pester in main test script before running Pester

--- test/test.ps1
+++ test/test.ps1
@@ -3,12 +3,18 @@
 Set-StrictMode -Version Latest
 $global:PesterDebugPreference_ShowFullErrors = $true
 
+# Install Pester if needed
+if (! ( Get-Module Pester -ErrorAction SilentlyContinue ) ) {
+    Install-Module Pester -Force -Scope CurrentUser
+}
+
+# Import our module
 Get-Module "$MODULE_NAME" | Remove-Module
 Import-Module "$APP_DIR/$MODULE_NAME.psd1" -Force
 
 # Run tests
 $res = Invoke-Pester -Script $APP_DIR -PassThru
 if ($res.FailedCount -gt 0) {
-    "$($res.FailedCount) tests failed."
+    "$( $res.FailedCount ) tests failed."
     exit 1
 }
